94058afbf595ccbcc0276de443d18282227dd559
Leo <67328482+Leonhard-Schwarz@users.noreply.github.com>
2023-12-01 22:44:42 +0100

    Add files via upload

---
+++ Advent-of-Code-day-1-part-2.js
@@ -0,0 +1,54 @@
+const input = [
+  "two1nine",
+  "eightwothree",
+  "abcone2threexyz",
+  "xtwone3four",
+  "4nineeightseven2",
+  "zoneight234",
+  "7pqrstsixteen",
+];
+
+function calculateCalibrationvValue(input) {
+  let combinedNumbers = [];
+  let number1Array = [];
+  let number2Array = [];
+  let finalNumber = 0;
+
+  const numberLookUpTable = {
+    nine: "9",
+    eight: "8",
+    seven: "7",
+    six: "6",
+    five: "5",
+    four: "4",
+    three: "3",
+    two: "2",
+    one: "1",
+  };
+
+  for (let i = 0; i < input.length; i++) {
+    const regex = /one|two|three|four|five|six|seven|eight|nine|\d/;
+    const regexReversed = /eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\d/;
+    let number1 = input[i].match(regex);
+    let number2 = input[i].split("").reverse().join("").match(regexReversed);
+    number1Array.push(number1[0]);
+    number2Array.push(number2[0].split("").reverse().join(""));
+
+    for (let key in numberLookUpTable) {
+      if (key == number1Array[i]) {
+        number1Array[i] = numberLookUpTable[key];
+      }
+    }
+    for (let key in numberLookUpTable) {
+      if (key == number2Array[i]) {
+        number2Array[i] = numberLookUpTable[key];
+      }
+    }
+    combinedNumbers.push(parseInt(number1Array[i] + number2Array[i]));
+    finalNumber += combinedNumbers[i];
+  }
+
+  return finalNumber;
+}
+
+console.log(calculateCalibrationvValue(input));
